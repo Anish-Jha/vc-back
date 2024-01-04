d1da62080bcd12e947ea967453da323f384b37ff
Anish-Jha <anishj545@gmail.com>
2024-01-04 16:20:49 +0530

    cor

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 const createError = require("http-errors");
 const morgan = require("morgan");
 require("dotenv").config();
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/api", require("./routes/api.route"));
 
