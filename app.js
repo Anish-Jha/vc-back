ecc3fc5097e437c3ac7226d9a61964393a678a87
Anish-Jha <anishj545@gmail.com>
2024-01-04 16:32:50 +0530

    file

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "http://localhost:8080",
-  })
-);
+app.use(cors({origin: "http://localhost:8080"}));
 
 app.use("/api", require("./routes/api.route"));
 
